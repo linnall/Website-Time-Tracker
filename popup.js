3e4f8ea4ac7f850c95857c2a2fe1b0d8836b5a9c
jasminekbal <59400398+jasminekbal@users.noreply.github.com>
2020-05-02 18:35:55 -0400

    Update popup.js

---
+++ popup.js
@@ -0,0 +1,12 @@
+let showGraphbtn = document.getElementById("showgraph");
+let clearBtn = document.getElementById("reset");
+
+clearBtn.onclick = function(element) {
+	chrome.storage.local.set({"tabTimesObject": "{}"}, function(){
+
+	});
+}
+
+showGraphbtn.onclick = function(element) {
+	
+}
