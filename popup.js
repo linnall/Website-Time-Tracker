0f7fe5685de08f63f428f8d08786345a8d50585a
Linna <linnaluo18@gmail.com>
2020-05-02 20:44:47 -0400

    resolving popup.js unecessary syntax

--- popup.js
+++ popup.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-//
-=======
 let showGraphbtn = document.getElementById("showgraph");
 let clearBtn = document.getElementById("reset");
 
@@ -13,4 +10,3 @@ clearBtn.onclick = function(element) {
 showGraphbtn.onclick = function(element) {
 	
 }
->>>>>>> 1062bf0bd1c04cef68db8a44b5e52e5a95e6d791
